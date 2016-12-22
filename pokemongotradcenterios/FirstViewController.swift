65b979c1bb69ffc32db48369d52388590b54cecf
RadiBarq <grayllow@gmail.com>
2016-12-22 22:21:14 +0200

    Initial Commit

---
+++ pokemongotradcenterios/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  pokemongotradcenterios
+//
+//  Created by Radi Barq on 12/22/16.
+//  Copyright © 2016 Radi Barq. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ pokemongotradcenterios/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  pokemongotradcenterios
+//
+//  Created by Radi Barq on 12/22/16.
+//  Copyright © 2016 Radi Barq. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
